2f62f1c071e8e3758145cd6aa38b784d2f3da234
nricco <niri8753@colorado.edu>
2022-09-01 14:33:04 -0600

    hw2 problem 1 finished

--- homework2.py
+++ homework2.py
@@ -5,3 +5,26 @@ Practice python flow control and functions following the
 discussions at https://www.pythoncheatsheet.org/
 """
 
+print("\nProblem 1")
+print("---------")
+print("Twinkle, twinkle, little star \n \t How I wonder what you are! \n" \
+      "\t\t Up above the world so high, \n \t\t Like a diamond in the sky. \n" \
+      "Twinkle, twinkle little star \n \t How I wonder what you are")
+    
+print("\nProblem 2")
+print("---------")
+
+print("\nProblem 3")
+print("---------")
+def getFileExtension(a_filename):
+     for i in a_filename:
+         print(i)
+getFileExtension('abc.java')
+print("\nProblem 4")
+print("---------")
+
+print("\nProblem 5")
+print("---------")
+
+print("\nProblem 6")
+print("---------")
